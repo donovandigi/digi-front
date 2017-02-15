0ebdc63df4e39dbb98c83798d5fb3e4e7b7c0b2d
Donovan Isherwood <donovan@matacore.co.za>
2017-02-15 23:11:27 +0100

    Loaded dev config from config files

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyCLR6FVpY-ON6fnbqV8zTn7513HUi2fOVU",
+        authDomain: "digi-front.firebaseapp.com",
+        databaseURL: "https://digi-front.firebaseio.com",
+        storageBucket: "digi-front.appspot.com",
+        messagingSenderId: "780181694799"
+    }
+}
+
+export default config;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,15 @@ import React from "react";
 import firebase from 'firebase';
 
 import Render from './Render';
+import config from '../../../config/dev';
 
+console.log(config);
 
 // app component
 export default class App extends React.Component {
   componentWillMount() {
-         // Initialize Firebase
-        const config = {
-            apiKey: "AIzaSyCLR6FVpY-ON6fnbqV8zTn7513HUi2fOVU",
-            authDomain: "digi-front.firebaseapp.com",
-            databaseURL: "https://digi-front.firebaseio.com",
-            storageBucket: "digi-front.appspot.com",
-            messagingSenderId: "780181694799"
-        };
-
-        firebase.initializeApp(config);
+        // Initialize Firebase
+        firebase.initializeApp(config.firebase);
   }
 
   // render
