4215b477fe81a9b653dff387f9b4abbcafd792b5
donovandigi <donovan@digiloads.co.za>
2017-02-19 20:27:05 +0100

    Setup firebase and CI (#3)
    * Updated the readme file
    
    * Loaded dev config from config files
    
    * Added firebase alias and set as default after success
    
    * Updated travis build
    
    * Added config for dev and prod

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyBRdvq35oWZUv_0ozKM0E2sl4a808I-SdQ",
+        authDomain: "digi-front-dev.firebaseapp.com",
+        databaseURL: "https://digi-front-dev.firebaseio.com",
+        storageBucket: "digi-front-dev.appspot.com",
+        messagingSenderId: "725491071488"
+    }
+}
+
+export default config;
